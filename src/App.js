513b4c8b2140cb271d53e264d46d839c0dd588cb
Katrina Hernandez <abqkatrina@gmail.com>
2020-01-23 00:15:33 -0700

    test one

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+<<<<<<< HEAD
 import React, {useState, useEffect} from "react";
 import axios from "axios";
 import Image from "./components/Image/Image.js";
 import Footer from "./components/Footer/Footer.js";
 import Header from "./components/Header/Header.js"
+=======
+//THIS THE THE APP JS PAGE FOR THE NASA PHOTO OF THE DAY PROJECT
+import React { useState, useEffect} from "react";
+import axios from "axios";
+import Header from "./Components/Header/header";
+import Screen from "./Components/Screen/screen";
+import Footer from "./Components/Footer/footer";
+>>>>>>> e274e1a42197ba9a4d034dd5891d60b6e1ff649f
 import "./App.css";
 
 function App() {
 
+<<<<<<< HEAD
   const [photos, setPhoto] = useState([]);
   
   useEffect(() => {
@@ -31,6 +41,13 @@ return (
     <Header/>
     <Image photos={photos.url}/>
     <Footer date={photos.date} title={photos.title}/>
+=======
+return (
+  <div className="App">
+    <Header />
+    <Screen data={response.api_key} />
+    <Footer data={response} />
+>>>>>>> e274e1a42197ba9a4d034dd5891d60b6e1ff649f
   </div>
 );
 }
